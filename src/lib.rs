0b7bee3dbd73156c52c6f12817786ad889d53620
ofuji-works <ofuji.works@gmail.com>
2024-03-11 00:36:37 +0900

    feat: implement get route

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,12 @@
 pub mod d1;
 pub mod repository;
 
+use d1::D1;
+use repository::{Options, Repository};
 use serde::{Deserialize, Serialize};
 use worker::*;
+
+const DB_NAME: &str = "test_db";
 
 #[derive(Debug, Deserialize, Serialize)]
 struct GenericResponse {
@@ -13,14 +17,21 @@ struct GenericResponse {
 #[event(fetch)]
 async fn main(req: Request, env: Env, _ctx: Context) -> Result<Response> {
     Router::new()
-        .get_async("/foo", handle_get)
-        .post_async("/bar", handle_post)
-        .delete_async("/baz", handle_delete)
+        .get_async("/get", handle_get)
+        .post_async("/post", handle_post)
+        .put_async("/update", handle_put)
+        .delete_async("/delete", handle_delete)
         .run(req, env)
         .await
 }
 
-pub async fn handle_get(_: Request, _ctx: RouteContext<()>) -> worker::Result<Response> {
+pub async fn handle_get(_: Request, ctx: RouteContext<()>) -> worker::Result<Response> {
+    let d1 = D1::from(ctx.env.d1(DB_NAME)?);
+    let options = Options::new(100);
+    let result = d1.get(options).await;
+
+    println!("{:?}", result);
+
     Response::from_json(&GenericResponse {
         status: 200,
         message: "You reached a GET route!".to_string(),
@@ -31,6 +42,13 @@ pub async fn handle_post(_: Request, _ctx: RouteContext<()>) -> worker::Result<R
     Response::from_json(&GenericResponse {
         status: 200,
         message: "You reached a POST route!".to_string(),
+    })
+}
+
+pub async fn handle_put(_: Request, _ctx: RouteContext<()>) -> worker::Result<Response> {
+    Response::from_json(&GenericResponse {
+        status: 200,
+        message: "You reached a PUT route!".to_string(),
     })
 }
 
